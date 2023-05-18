8795b4eb48f36ea9e86c278832374fa6e252552b
PoornaU <62684793+PoornaU@users.noreply.github.com>
2023-05-18 10:02:15 +1000

    Added

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', router);
 
+io.on('connection' , (socket)=>{
+    console.log('connected a client');
+    socket.on('disconnect' ,()=>{
+        console.log('a client has disconnected');
+    });
+
+    setInterval(()=>{
+        socket.emit('number' + parseInt(Math.random() * 10));
+    }, 1000);
+});
+
 var port = process.env.port || 3000;
 app.listen(port, () => {
     console.log('App listening to: ' + port);
